9896a4ef1602b29768ef981bf4af986a154cb259
Bartosz456 <bartoszsz14@gmail.com>
2023-04-20 19:17:15 +0200

    tablica dynamiczna

---
+++ tablica-dynamiczna.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int* tab;
+
+	tab = new int[4];
+
+	for (int i = 0; i < 4; i++)
+	{
+		tab[i] = i + 4;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << tab[i] << " ";
+	}
+
+	delete[]tab;
+}
